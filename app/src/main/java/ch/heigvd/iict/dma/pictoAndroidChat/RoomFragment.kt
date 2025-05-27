8a14ac9bd55287e44667941b48cc7d6b5155c908
Tasticoco <114155288+Tasticoco@users.noreply.github.com>
2025-05-27 11:20:16 +0200

    Room with canva

--- app/src/main/java/ch/heigvd/iict/dma/pictoAndroidChat/RoomFragment.kt
+++ app/src/main/java/ch/heigvd/iict/dma/pictoAndroidChat/RoomFragment.kt
@@ -7,23 +7,25 @@ import androidx.fragment.app.FragmentActivity
 import io.ak1.drawbox.DrawBox
 import io.ak1.drawbox.rememberDrawController
 import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.fillMaxSize
 
 class RoomFragment : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
         super.onCreate(savedInstanceState, persistentState)
         setContentView(R.layout.activity_room)
-
-        val controller = rememberDrawController()
-
-        DrawBox(
-            drawController = controller, modifier = Modifier.t,
-            backgroundColor = TODO(),
-            bitmapCallback = TODO(),
-            trackHistory = TODO()
-        )
         
         val canva = findViewById<ComposeView>(R.id.canva)
         canva.setContent {
+            val controller = rememberDrawController()
+
+            DrawBox(
+                drawController = controller, modifier = Modifier.fillMaxSize(),
+                bitmapCallback = { imageBitmap, error ->
+                    imageBitmap?.let {
+                       // TODO()
+                    }
+                }
+            )
         }
 
     }
